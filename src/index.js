576e6e732ab49165df3be974ea1744ff8524569a
ahmetmbilgin <ahmet.m.bilgin@hotmail.com>
2023-01-02 06:18:00 +0300

    düzenleme

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import './index.css';
 import Coffees from './pages/coffees';
 import Foods from './pages/foods';
 import HomePage from './pages/homePage';
-import Login from './pages/login/login';
 import NoPage from './pages/noPage';
 import Signup from './pages/signup/signup';
 import Snacks from './pages/snacks';
 import User from './pages/user/user';
+import "@blueprintjs/icons/lib/css/blueprint-icons.css"
+import "@blueprintjs/core/lib/css/blueprint.css"
+import "normalize.css/normalize.css"
+import { FocusStyleManager } from "@blueprintjs/core";
 
+FocusStyleManager.onlyShowFocusOnTabs();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
@@ -19,7 +23,6 @@ root.render(
         <Routes>
             <Route exact path="/" element={<HomePage />} />
             <Route exact path="user/:id" element={<User />} />
-            <Route exact path='login' element={<Login />} />
             <Route exact path='signup' element={<Signup />} />
             <Route exact path='foods' element={<Foods />} />
             <Route exact path='coffees' element={<Coffees />} />
